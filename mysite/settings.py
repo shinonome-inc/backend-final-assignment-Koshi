969731759a4bcbf054224a11bc79d98d6191fa27
yoskoshi <8423koshi@ezweb.ne.jp>
2023-08-19 17:57:11 +0900

    insert LOGOUT_URL in mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "accounts:login"
+LOGOUT_URL = "accounts:logout"
 LOGIN_REDIRECT_URL = "tweets:home"
 LOGOUT_REDIRECT_URL = "accounts:login"
