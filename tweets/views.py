b779835f6ce0f5b1851af33d8979e99f041bb553
Yoshida-koshi <koshi2278@outlook.jp>
2023-08-21 16:39:40 +0900

    create tweet list in HomeView

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, ListView
 
 from .forms import TweetForm
+from .models import Tweet
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(LoginRequiredMixin, ListView):
     template_name = "tweets/home.html"
+    model = Tweet
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # ツイートを投稿日時の降順に並び替える
+        context["tweet_list"] = Tweet.objects.order_by("-create_date")
+        return context
 
 
 class TweetCreateView(LoginRequiredMixin, CreateView):
