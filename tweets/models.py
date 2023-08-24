4a07760d8596ee6c3292bc0ee38d879b5eede190
Yoshida-koshi <koshi2278@outlook.jp>
2023-08-24 13:06:40 +0900

    change from create_date to created_at

--- tweets/models.py
+++ tweets/models.py
@@ -8,10 +8,10 @@ User = get_user_model()
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=50)
-    create_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.content
 
     class Meta:
-        ordering = ["-create_date"]
+        ordering = ["-created_at"]
