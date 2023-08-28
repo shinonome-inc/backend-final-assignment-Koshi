24517fef4aac548af469ce1daa8106ce49790836
yoskoshi <8423koshi@ezweb.ne.jp>
2023-08-28 13:51:37 +0900

    create FriendShip Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 class User(AbstractUser):
     email = models.EmailField()
 
 
-# class FriendShip(models.Model):
+class FriendShip(models.Model):
+    following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    
+    class Meta:
+        ordering = ["-created_at"]
