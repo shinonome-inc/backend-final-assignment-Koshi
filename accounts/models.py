a4aa75f6f16930951917c840513919f957f50c84
yoskoshi <8423koshi@ezweb.ne.jp>
2023-08-28 13:52:24 +0900

    fix indent

--- accounts/models.py
+++ accounts/models.py
@@ -11,6 +11,6 @@ class FriendShip(models.Model):
     following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
     follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
     created_at = models.DateTimeField(default=timezone.now)
-    
+
     class Meta:
         ordering = ["-created_at"]
