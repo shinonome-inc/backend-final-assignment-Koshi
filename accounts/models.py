746220e6487bb2a5a9ddd84d8e31a241113addf8
yoskoshi <8423koshi@ezweb.ne.jp>
2023-08-30 16:20:55 +0900

    change from User to settings.AUTH_USER_MODEL

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from mysite import settings
+
 
 class User(AbstractUser):
     email = models.EmailField()
 
 
 class FriendShip(models.Model):
-    following = models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
-    follower = models.ForeignKey(User, related_name="follower", on_delete=models.CASCADE)
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="following", on_delete=models.CASCADE)
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="follower", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
