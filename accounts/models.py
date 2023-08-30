8d18b494508d59f2c982ad91e6edb4315a60c642
yoskoshi <8423koshi@ezweb.ne.jp>
2023-08-30 16:28:30 +0900

    change related_name from following and follower to followings and followers

--- accounts/models.py
+++ accounts/models.py
@@ -9,8 +9,8 @@ class User(AbstractUser):
 
 
 class FriendShip(models.Model):
-    following = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="following", on_delete=models.CASCADE)
-    follower = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="follower", on_delete=models.CASCADE)
+    following = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="followings", on_delete=models.CASCADE)
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="followers", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
